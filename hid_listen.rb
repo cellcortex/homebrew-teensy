57e4e3aa115952a1537517829a6586fbad015020
Thomas Kroeber <cellcortex@gmail.com>
2015-09-18 00:40:27 +0200

    added hid_listen script from teensy

--- hid_listen.rb
+++ hid_listen.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class HidListen < Formula
   homepage 'https://www.pjrc.com/teensy/hid_listen.html'
-  version '100'
+  version '0.2'
   url 'https://www.pjrc.com/teensy/hid_listen.mac'
   sha1 'fc8b20ca9e13511fda30c77dee4f32c2072c3e08'
 
   def install 
     ohai 'Copying hid_listen...'
     chmod 0555, "hid_listen.mac"
-#    system 'cp', '-v', 'hid_listen.mac', "#{prefix}/"
+    system 'cp', '-v', 'hid_listen.mac', "#{prefix}/"
     bin.install "hid_listen.mac"
     bin.install_symlink libexec/"hid_listen.mac"
   end
