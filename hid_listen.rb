19d7da5b2d46367e986276a23426773f899ab3f3
Thomas Kroeber <cellcortex@gmail.com>
2015-09-18 00:03:46 +0200

    added hid_listen script from teensy

--- hid_listen.rb
+++ hid_listen.rb
@@ -4,7 +4,7 @@ class HidListen < Formula
   homepage 'https://www.pjrc.com/teensy/hid_listen.html'
   version '20150917'
   url 'https://www.pjrc.com/teensy/hid_listen.mac'
-  sha1 '7f2d8c3644cd76da1b78d17a0770c8c0efbff957'
+  sha1 'fc8b20ca9e13511fda30c77dee4f32c2072c3e08'
 
   def install 
     ohai 'Copying binaries...'
