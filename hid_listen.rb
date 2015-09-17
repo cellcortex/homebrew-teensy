8d3627d42e98dcfda8bc683ddb4ba9d44ae03288
Thomas Kroeber <cellcortex@gmail.com>
2015-09-18 00:25:29 +0200

    added hid_listen script from teensy

--- hid_listen.rb
+++ hid_listen.rb
@@ -7,10 +7,11 @@ class HidListen < Formula
   sha1 'fc8b20ca9e13511fda30c77dee4f32c2072c3e08'
 
   def install 
-    ohai 'Copying binaries...'
+    ohai 'Copying hid_listen...'
+    chmod 0555, "hid_listen.mac"
 #    system 'cp', '-v', 'hid_listen.mac', "#{prefix}/"
     bin.install "hid_listen.mac"
-    chmod 0555, Dir["#{bin}/hid_listen.mac"]
+    bin.install_symlink libexec/"hid_listen.mac"
   end
 end
 
