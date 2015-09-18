57a5c6e1c622d26528feae2f06cb3ff39f68e38b
Thomas Kroeber <thomas.kroeber@here.com>
2015-09-18 11:16:06 +0200

    Teensy loader added

---
+++ teensy_loader_cli.rb
@@ -0,0 +1,34 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/homebrew/master/frames
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class TeensyLoaderCli < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/PaulStoffregen/teensy_loader_cli"
+  version "0.1"
+  sha256 "18ba669c941f7ba5662f0089c8e132f78d8d648e29cd939f67db33ee2ffa97cf"
+
+  # depends_on "cmake" => :build
+  depends_on :libusb # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # system "cmake", ".", *std_cmake_args
+    system "make", "teensy_loader_cli" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test teensy`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
